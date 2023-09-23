d44c65f99b025e9601112b0ed24e22877fe57fa1
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2023-09-23 12:08:32 +0300

    add my changes

---
+++ infrastructure/setup-instance.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# install docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# create github user
+sudo mkdir -p /home/app
+sudo useradd --no-create-home --home-dir /home/app --shell /bin/bash github
+sudo usermod --append --groups docker github
+sudo usermod --append --groups docker ubuntu
+sudo chown github:github -R /home/app
+
+github_pubkey='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHbh3ETq9fVsTym2UW8Fqk3D1NziO5/C77m6KM9h4ROS khaledez@me.com'
+
+sudo -u github sh -c "mkdir -p /home/app/.ssh && echo $github_pubkey > /home/app/.ssh/authorized_keys"
+
+sudo reboot
